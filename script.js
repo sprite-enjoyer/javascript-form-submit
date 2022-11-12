789db4129608a9528e92916b52b17386a699b4ee
sprite-enjoyer <samkharadze.saba01@gmail.com>
2022-11-12 22:46:33 +0400

    overlay functional

--- script.js
+++ script.js
@@ -5,13 +5,62 @@ const dob = document.querySelector(".calendar");
 const sex = document.querySelector(".dropdown");
 const notes = document.querySelector(".notes");
 const btn = document.querySelector(".btn");
+const table = document.querySelector(".table");
 
 btn.addEventListener("click", (e) => {
     e.preventDefault();
-});
 
-dob.addEventListener("change", () => console.log(dob.value))
-firstName.addEventListener("keyup", () => {console.log(localStorage.getItem("dob"))})
+    const fn = firstName.value;
+    const ln = lastName.value;
+    const ad = address.value;
+    const date = dob.value;
+    const sx = sex.value;
+    const note = notes.value;
+
+    if (!firstAndLastNameValidator(fn) || !firstAndLastNameValidator(ln) || !addressValidator(ad)) return;
+    
+    const row = table.insertRow(-1);
+    row.style.cursor = "pointer";
+
+    if (!localStorage.getItem("index")) localStorage.setItem("index", "-1");
+    localStorage.setItem("index", (parseInt(localStorage.getItem("index")) + 1).toString());
+
+    row.addEventListener("click", () => {
+        const popup = document.querySelector(".popup");
+        const noteContainer = document.querySelector(".note-container");
+        const textContainer = document.querySelector(".text-container");
+        const textNode = document.createTextNode(note);
+        if (noteContainer.childElementCount <= 2) textContainer.replaceChild(textNode, textContainer.firstChild);
+
+        const btn = document.querySelector(".popup-btn");
+        btn.addEventListener("click", () => {
+            popup.style.visibility = "hidden";
+            noteContainer.style.visibility = "hidden";
+
+        });
+
+        popup.style.visibility = "visible";
+        noteContainer.style.visibility = "visible";
+    });
+
+    const popup = document.querySelector(".popup");
+    popup.addEventListener("keyup", (e) => {
+        console.log("here")
+        if (e.key === "Escape") console.log("escape pressed")
+    })
+
+
+    let cell = row.insertCell(0);
+    let text = document.createTextNode(localStorage.getItem("index"));
+    cell.appendChild(text);
+
+    [fn, ln, ad, date, sx].forEach((element, i) => {
+        cell = row.insertCell(i + 1);
+        text = document.createTextNode(element);
+        cell.appendChild(text);
+    })
+
+});
 
 validateField(
     firstName, 
@@ -43,6 +92,11 @@ validateField(
     "Maximum of 35 characters allowed"
 );
 
+loadFields(
+    [firstName, lastName, address, dob, sex, notes], 
+    ["firstName", "lastName", "address", "dob", "sex", "notes"]
+    );    
+
 saveFields(
     [firstName, lastName, address, notes], 
     "keyup", 
@@ -55,7 +109,3 @@ saveFields(
     ["dob", "sex"]
 );    
 
-loadFields(
-    [firstName, lastName, address, dob, sex, notes], 
-    ["firstName", "lastName", "address", "dob", "sex", "notes"]
-    );    
